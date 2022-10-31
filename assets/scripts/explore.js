4c07b9829ce767e4c2fb0700327ef63430b2c0dc
liquidbunx <dcv004@ucsd.edu>
2022-10-30 19:01:45 -0700

    finish explore and expose js files

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -2,6 +2,59 @@
 
 window.addEventListener('DOMContentLoaded', init);
 
+
+const synth = window.speechSynthesis;
+const voice_selector = document.getElementById('voice-select');
+const tts_input = document.getElementById('text-to-speak');
+const talk_button = document.querySelector('button');
+let voices = [];
 function init() {
-  // TODO
+
+  //Function to populate the voices on dropdown list
+  function populateVoiceList() {
+	// retrieve a list of the voices available
+    voices = synth.getVoices();
+    for (let i = 0; i < voices.length; i++) {
+      const option = document.createElement('option');
+      option.textContent = `${voices[i].name} (${voices[i].lang})`;
+
+      option.setAttribute('data-lang', voices[i].lang);
+      option.setAttribute('data-name', voices[i].name);
+      voice_selector.appendChild(option); 
+    } 
+  };
+
+  populateVoiceList();    
+  if (speechSynthesis.onvoiceschanged !== undefined) {
+    speechSynthesis.onvoiceschanged = populateVoiceList;
+  }
+
+  var face_swap = document.querySelector('img');
+  talk_button.addEventListener('click', (event) => {
+	// Stop the form submitting
+    event.preventDefault();
+
+	//Create instance that contains the text from the text input. 
+    const utterThis = new SpeechSynthesisUtterance(tts_input.value);
+	//Set the utterance's voice to the voice selected in the <select> element
+    const selectedOption = voice_selector.selectedOptions[0].getAttribute('data-name');
+    for (let i = 0; i < voices.length ; i++) {
+      if (voices[i].name === selectedOption) {
+        utterThis.voice = voices[i];
+      }
+    }
+	//Start the utterance speaking
+    synth.speak(utterThis);
+
+	//OWhile the synthesizer is speaking, the face should swap to being open mouthed. 
+	function while_speaking() {
+	  if (synth.speaking) {
+		  face_swap.src = `assets/images/smiling-open.png`;
+	  }
+	  else {
+	  face_swap.src = `assets/images/smiling.png`;
+	  }
+	}
+	setInterval(while_speaking);
+  });
 }
--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,61 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+
+  // Variable for selecting different horns
+  const horn_selector = document.querySelector("#horn-select");
+  // Select the different audio type 
+  let audio_type = document.querySelector('audio');
+  // Change image and volume type for different horn options
+  horn_selector.addEventListener('change', (event) => {
+    // Look at the "value" tag of the horn options
+    let horn_type = event.target.value;
+    // Use image tag
+    let img_type = document.querySelector("img");
+
+    if (horn_type == 'air-horn') {
+        img_type.src = 'assets/images/air-horn.svg';
+        audio_type.src = 'assets/audio/air-horn.mp3';   
+    }
+    else if (horn_type == 'car-horn') {
+        img_type.src = 'assets/images/car-horn.svg';
+        audio_type.src = 'assets/audio/car-horn.mp3';
+    }   
+    else if (horn_type == 'party-horn') {
+        img_type.src = 'assets/images/party-horn.svg';
+        audio_type.src = 'assets/audio/party-horn.mp3';
+    }
+
+  });
+
+
+  const volume_slider = document.querySelector('#volume');
+  // Change volume icon based on position (i.e. "value") of slider
+  volume_slider.addEventListener('change', (event) => {
+    // Use the "value" tag inside div "volume-controls"
+    let slider = event.target.value;
+    // Use the div image tag
+    let audio_icon = document.querySelector('div img')
+    if (slider == 0) {
+      audio_icon.src = `assets/icons/volume-level-0.svg`;
+    } else if (slider < 33) {
+      audio_icon.src = `assets/icons/volume-level-1.svg`;
+    } else if (slider < 67) {
+      audio_icon.src = `assets/icons/volume-level-2.svg`;
+    } else {
+      audio_icon.src = `assets/icons/volume-level-3.svg`;
+    }
+    audio_type.volume = volume_slider.value / 100;
+  });
+
+  
+  const play_sound = document.querySelector('button');
+  const jsConfetti = new JSConfetti();
+  //Button to play sound. Also display confetti if party horn is selected. 
+  play_sound.addEventListener('click', (event) => {
+    audio_type.play();
+    if (horn_selector.value == 'party-horn') {
+      jsConfetti.addConfetti();
+    }
+  });
 }
